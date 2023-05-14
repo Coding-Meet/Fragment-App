22c27cdf0a2b4c7f313e1c7a482551da4cee48d2
coding meet <codingmeet26@gmail.com>
2023-05-14 21:24:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragmentapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.fragmentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment1Btn = findViewById<Button>(R.id.fragment1Btn)
+        val fragment2Btn = findViewById<Button>(R.id.fragment2Btn)
+
+        // here application start but default fragment not set
+        //
+        replaceFragment(Fragment1())
+
+        fragment1Btn.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+        fragment2Btn.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container,fragment)
+            .commit()
+    }
+}
